28cff731634de37fbcf4d261e8b78ebafcdc12fe
ymadz <138951434+ymadz@users.noreply.github.com>
2024-08-08 08:14:54 +0800

    fes
    fsd

---
+++ webdev 2/dom manipulation/script.js
@@ -0,0 +1,14 @@
+const body = document.body
+// const div = document.createElement(`div`)
+// const div2 = document.createElement(`div`)
+// div.innerText = "hello world"
+// div2.innerHTML = "<h1>big boi</h1>"
+// body.append(div)
+// body.append(div2)
+
+// const spanBye = document.querySelector(`#bye`)
+// spanBye.remove()
+
+const spanHi = document.querySelector(`#hi`)
+spanHi.style.background = `red`;
+spanHi.removeAttribute(background);
