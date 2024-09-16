3e216429ad12b030e4bd27f348e66a65e9bbec17
Ahmad Yahiya <138951434+ymadz@users.noreply.github.com>
2024-09-16 23:59:24 +0800

    Delete webdev 2/library directory

--- webdev 2/library/classes/function.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function clean_input($input) {
-        $input = trim($input);
-        $input = stripslashes($input);
-        $input = htmlspecialchars($input);
-        return $input;
-    }
